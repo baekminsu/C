3df9e38690bdefccc9ebbaad2d2f2b0ddb6a5a98
baekminsu <110341040+baekminsu@users.noreply.github.com>
2023-01-20 20:54:41 +0900

    Create al_analysis_practice3_2_0.1.c

---
+++ datastructure/al_analysis_practice3_2_0.1.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h> 
+#define _CRT_SECURE_NO_WARNINGS
+#pragma warning(disable:4996)
+/*함수 prefixAverages1과 prefixAverages2, 그리고 이들을 테스트할 수 있는 main
+함수를 구현하여 아래 테스트를 수행하라.*/
+
+int prefixAverages1(int *x, int n){
+
+
+}
+int prfixAverages2{
+
+
+}
+int main() {
+	int n;
+	scanf("%d", &n);
+	int* x;
+	x = (int*)malloc(sizeof(int) * n);
+	
+	for (i = 0; i < n; i++) {
+
+	}
+
+	free(x);
+	return 0;
+}
