debcead321215b0417951caf7bc564873115a2b0
baekminsu <110341040+baekminsu@users.noreply.github.com>
2023-05-10 23:55:46 +0900

    Create stack2_0.1.c

---
+++ datastructure/stack2_0.1.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#define _CRT_SECURE_NO_WARNINGS
+#pragma warning(disable:4996)
+
+int main() {
+
+	char string[1000];
+	scanf("%s", string);
+
+
+
+
+	
+	return 0;
+}
